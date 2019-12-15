7b16eb8755aa7b56e3c541a42288a11a46e8a005
lucasribeiro163 <44579909+lucasribeiro163@users.noreply.github.com>
2019-12-15 17:14:28 +0000

    Faltava uma oagina

---
+++ Src/pages/house_reservations.php
@@ -0,0 +1,22 @@
+<?php 
+  include_once('../includes/session.php');
+  include_once('../templates/tpl_common.php');
+  include_once('../templates/tpl_auth.php');
+  include_once('../templates/tpl_lists.php');
+  include_once('../database/db_list.php');
+
+  // Verify if user is logged in
+  if (!isset($_SESSION['username']))
+    die(header('Location: login.php'));
+
+  $house = $_GET['house'];
+  //gets user id
+  $userId = getPersonId($_SESSION['username']);
+
+  $reservations = getReservations($house);
+
+
+  draw_header($_SESSION['username']);
+  draw_house_reservations($reservations, $house);
+  draw_footer();
+?>
