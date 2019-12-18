296b801f35ba2dc05028e5db81a2138d7f3134f1
Andreia Gouveia <andiigouveia@hotmail.com>
2019-12-18 00:16:28 +0000

    added small aspect

--- Src/templates/tpl_lists.php
+++ Src/templates/tpl_lists.php
@@ -58,7 +58,7 @@
 <?php } ?>
 
 
-<?php function draw_house_ad($house, $lists) {
+<?php function draw_house_ad($loged_in_user,$house, $lists) {
 
   foreach($lists as $list){
     foreach ($list['list_items'] as $item){
@@ -116,6 +116,9 @@
     <button type="button" id="ShowComments" onclick="getComments()" >Show Comments</button>
     <ul id="houseComments" ></ul>
     </section>
+
+    <button type="button" id="ShowComments" onclick="getComments()" >Show Comments</button>
+
     </section>
 <?php }
 
